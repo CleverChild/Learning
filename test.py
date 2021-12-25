e4446a9f507ea8aff64725ba5e509ca3de59b79b
CleverChild <imminseob2@gmail.com>
2021-12-26 02:12:01 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+#s = 'aabbbbcccd'
+s = "abcabcabcabcdededededede"
+
+result = s[0]  # 첫번째 값을 결과에 넣는다
+count  = 0
+
+for st in s:
+    if st == result[-1]:       #result[-1] == prev
+        count += 1
+    else:
+        result += str(count) + st
+        count = 1
+result += str(count)
+
+print(result)
