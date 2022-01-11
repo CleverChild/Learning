4f33956f44fa948c00aca957555af2c017d036bf
CleverChild <96661246+CleverChild@users.noreply.github.com>
2022-01-11 13:57:57 +0900

    Add files via upload

---
+++ 2021/University/Andriod/OpenGL/FinalTerm/Animation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.minseoblim_201621228_final_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    private GLSurfaceView gLView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+    }
+}
